ecf3529cc08e9eb387c3b06d8aae832b47617641
haritonch <haritonch@yahoo.com>
2025-01-21 22:05:52 +0200

    split into multiple files

---
+++ query1.py
@@ -0,0 +1,40 @@
+from pyspark.sql.functions import col, when
+from timed import timed
+
+@timed
+def query1_dataframe(crime_data_df):
+    crime_data_df \
+        .filter(crime_data_df["Crm Cd Desc"].contains("AGGRAVATED ASSAULT")) \
+        .withColumn(
+            "Vict Age Group",
+            when(col("Vict Age") < 18, "<18")
+            .when((18 <= col("Vict Age")) & (col("Vict Age") <= 24), "18-24")
+            .when((25 <= col("Vict Age")) & (col("Vict Age") <= 64), "25-64")
+            .when(64 < col("Vict Age"), ">64")) \
+        .groupBy("Vict Age Group") \
+        .count() \
+        .orderBy("count", ascending=False) \
+        .show()
+
+
+@timed
+def query1_rdd(crime_data_df):
+    def age_group(row):
+        if int(row["Vict Age"]) < 18:
+            return "<18"
+        elif 18 <= int(row['Vict Age']) <= 24:
+            return "18-24"
+        elif 25 <= int(row['Vict Age']) <= 64:
+            return "25-64"
+        else:
+            return ">64"
+
+    crime_data_df \
+        .rdd \
+        .filter(lambda x: "AGGRAVATED ASSAULT" in x["Crm Cd Desc"]) \
+        .map(lambda x: (age_group(x), 1)) \
+        .reduceByKey(lambda a, b: a + b) \
+        .sortBy(lambda x: -x[1]) \
+        .toDF(["Vict Age Group", "count"]) \
+        .show()
+        
---
+++ query2.py
@@ -0,0 +1,63 @@
+from pyspark.sql.functions import rank, col
+from pyspark.sql.window import Window
+from timed import timed
+
+
+@timed
+def query2_sql(spark_session, crime_data_df):
+    crime_data_df.createOrReplaceTempView("crime_data")
+    query = """
+        SELECT
+            year,
+            precinct,
+            closed_case_rate,
+            RANK() OVER (PARTITION BY year ORDER BY closed_case_rate DESC) as `#`
+        FROM (
+            SELECT 
+                YEAR(`DATE OCC`) as year,
+                `AREA NAME` as precinct,
+                COUNT(CASE WHEN `Status Desc` NOT IN ('Invest Cont', 'UNK') THEN 1 END) / COUNT(*) as closed_case_rate
+            FROM crime_data
+            GROUP BY `AREA NAME`, YEAR(`DATE OCC`)
+        )
+    """
+    result = spark_session.sql(query)
+    result.show()
+
+
+@timed
+def query2_rdd(crime_data_df):
+    def processed_indicator(row):
+        if row["Status Desc"] in ("Invest Cont", "UNK"):
+            return 0
+        else:
+            return 1
+
+    crime_data_df \
+        .rdd \
+        .map(lambda row: ((row["AREA NAME"], row["DATE OCC"].year), (processed_indicator(row), 1))) \
+        .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])) \
+        .mapValues(lambda x: x[0] / x[1]) \
+        .map(lambda x: (x[0][1], x[0][0], x[1])) \
+        .toDF([
+            "year",
+            "precinct",
+            "closed_case_rate",
+        ]) \
+        .withColumn("#", rank().over(Window.partitionBy("year").orderBy(col("closed_case_rate").desc()))) \
+        .orderBy("year", "#") \
+        .show()
+
+
+def save_crime_data_to_single_parquet_file(crime_data_df):
+    crime_data_df \
+        .repartition(1) \
+        .write \
+        .mode("overwrite") \
+        .parquet("s3://groups-bucket-dblab-905418150721/group3/crime_data.parquet")
+
+
+def load_crime_data_from_single_parquet_file(spark_session):
+    return spark_session \
+        .read \
+        .parquet("s3://groups-bucket-dblab-905418150721/group3/crime_data.parquet")
